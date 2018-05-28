0f7488e52bd9c5ce58fe4f9e406df94330dada3a
Joel O'Connor <joconno07@gmail.com>
2018-05-28 15:16:01 -0600

    Api object being pulled neeeds to connect with cleaners appropriately

--- src/lib/App.js
+++ src/lib/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
-import data from './mock-data.js';
+// import data from './mock-data.js';
 import dataCleaner from './dataCleaner';
 import CurrentWeather from './Current-Weather.js';
 import sevenHourForecastCleaner from './sevenHourDataCleaner';
@@ -23,21 +23,33 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const currentWeatherData = dataCleaner(data);
-    this.setState({
-      currentWeather: dataCleaner(data),
-      sevenHourForecast: sevenHourForecastCleaner(data),
-      tenDayForecast: tenDayCleaner(data),
+    fetch('http://api.wunderground.com/api/81347f06b321e144//conditions/geolookup/hourly/forecast10day/q/CO/denver.json')
+    .then(data => data.json())
+    .then(parsedData => console.log(parsedData))
+    .then(parsedData => {
+        this.setState({
+        currentWeather: dataCleaner(parsedData),
+        sevenHourForecast: sevenHourForecastCleaner(parsedData),
+        tenDayForecast: tenDayCleaner(parsedData),
+      })
     })
-    // this.getData()  
+    .catch(err => console.log('err-nothing came back', err))
   }
 
+
+    // this.setState({
+    //   currentWeather: dataCleaner(data),
+    //   sevenHourForecast: sevenHourForecastCleaner(data),
+    //   tenDayForecast: tenDayCleaner(data),
+    // })
+
+
+
   // getData() {
-  //   fetch('http://api.wunderground.com/api//conditions/q/CO/denver.json')
-  //     .then(response => response.json())
-  //     .then(data => dataCleaner(data))
-  //     .then(cleanData => console.log(cleanData))
-  //     .catch(david => console.log(david))
+  //   fetch('http://api.wunderground.com/api/81347f06b321e144/conditions/q/CO/denver.json')
+  //   .then(data => data.json())
+  //   .then(data => console.log(data))
+  //   .catch(err => console.log('err-nothing came back'))
   // }
 
   render() {
