6befe3f57c59f8e52ce2bce36c63022351b8fd8f
Kevin Krom <kevinkrom787@gmail.com>
2018-05-29 11:17:27 -0600

    Add api and hid key

--- src/lib/dataCleaner.js
+++ src/lib/dataCleaner.js
@@ -1,19 +1,20 @@
-const dataCleaner = (data) => {
+import React from 'react';
 
-  const days = data.forecast.simpleforecast.forecastday
-  const currentTemp = data.current_observation.temperature_string
-  const location = data.current_observation.display_location.full
-  const getIt = days.find(day => day.date.day === 20 && day.date.month === 12 && day.date.year === 2017);
+const dataCleaner = (parsedData) => {
 
+  const days = parsedData.forecast.simpleforecast.forecastday
+  const currentTemp = parsedData.current_observation.temperature_string
+  const location = parsedData.current_observation.display_location.full
+  
   const getInfo = {
-    high: getIt.high.fahrenheit,
-    low: getIt.low.fahrenheit,
-    date: getIt.date.pretty,
-    conditions: getIt.conditions,
-    avehumidity: getIt.avehumidity,
+    high: days[0].high.fahrenheit,
+    low: days[0].low.fahrenheit,
+    date: days[0].date.pretty,
+    conditions: days[0].conditions,
+    avehumidity: days[0].avehumidity,
     location,
     currentTemp,
-    icon: getIt.icon_url
+    icon: days[0].icon_url
   }
 
   return getInfo 
