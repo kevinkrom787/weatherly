05b236d2a4d3f7814a8668df094776895ef4d250
Kevin Krom <kevinkrom787@gmail.com>
2018-05-30 14:26:05 -0600

    Add testing for localStorage

---
+++ src/test/Seven.Hour.Forecast.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+global.localStorage = {}
+
+import sevenHourForecast from '../lib/Seven-Hour-Forecast';
+import CurrentWeather from '../lib/Current-Weather';
+import tenDayForecast from '../lib/Ten-Day-Forecast';
+
+describe('sevenHourForecast Test', () => {
+
+  let renderedSevenHourForecast;
+  beforeEach(() => renderedApp = shallow(<sevenHourForecast />));
+
+  describe('sevenHourForecast should return an hour prop', () => {
+
+
+  })
+})
